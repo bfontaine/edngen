74bc07fbeabbbc0f3adf088d1f24d10b3d3a7805
Baptiste Fontaine <b@ptistefontaine.fr>
2019-12-15 22:33:16 +0100

    make the code faster
    Main (internal) changes:
    
    * Use `binding` only once instead of once per line
    * Use `prn` instead of `println` + `pr-str`

--- src/edngen/core.clj
+++ src/edngen/core.clj
@@ -4,20 +4,33 @@
 
 ;; Based on
 ;; https://github.com/clojure/clojure/blob/71511b7800e18c83377a322f43585a853b303698/test/clojure/test_clojure/edn.clj#L17
+
+(defmacro with-safe-print
+  [& body]
+  `(binding [*print-length* nil
+             *print-dup* nil
+             *print-level* nil]
+    ~@body))
+
+(def ^:private gen*
+  cgen/ednable)
+
 (defn gen
+  "Return a random EDN structure in one string."
   []
-  (binding [*print-length* nil
-            *print-dup* nil
-            *print-level* nil]
-    (pr-str (cgen/ednable))))
+  (with-safe-print
+    (pr-str (gen*))))
 
 (defn -main
   [& args]
-  (if-let [n (when-let [arg (first args)]
-               (try
-                 (Long/parseLong arg)
-                 (catch NumberFormatException _ nil)))]
-    (doseq [_ (range n)]
-      (println (gen)))
-    (while true
-      (println (gen)))))
+  (let [n (when-let [arg (first args)]
+            ;; Abort with an exception if this is not a valid number
+            (Long/parseLong arg))]
+    (with-safe-print
+      (if n
+        (dotimes [_ n]
+          (prn (gen*)))
+        (while true
+          (prn (gen*)))))))
+
+(ns-unmap *ns* 'with-safe-print)
